6a94e0b5611c05c97e3bc818d5b32aa643bc22e8
Abby Ashton <ashtwj1@student.op.ac.nz>
2026-06-02 20:28:43 +1200

    Added unit testing for postfix parser

---
+++ Calculator.UnitTesting/IsPostfixParsedCorrectly.cs
@@ -0,0 +1,69 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.UnitTesting;
+
+[TestClass]
+public class IsPostFixParsedCorrectly
+{
+    private PostfixParser _Parser;
+
+    public IsPostFixParsedCorrectly()
+    {
+        _Parser = new PostfixParser();
+    }
+
+    [TestMethod]
+    [DataRow(new object[] { 1, 1, 2 })]
+    [DataRow(new object[] { 0, 0, 0 })]
+    [DataRow(new object[] { 3, 6, 9 })]
+    [DataRow(new object[] { 10, 11, 21 })]
+    [DataRow(new object[] { 100, 1002, 1102 })]
+    public void IsAdditionCorrect(int a, int b, int expectedResult)
+    {
+        _Parser.Expression = new string[] { a.ToString(), b.ToString(), "+" };
+        int result = _Parser.Evaluate();
+
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [TestMethod]
+    [DataRow(new object[] { 1, 1, 1 })]
+    [DataRow(new object[] { 0, 0, 0 })]
+    [DataRow(new object[] { 10, 10, 100 })]
+    [DataRow(new object[] { 3, 2, 6 })]
+    public void IsMultiplicationCorrect(int a, int b, int expectedResult)
+    {
+        _Parser.Expression = new string[] { a.ToString(), b.ToString(), "*" };
+        int result = _Parser.Evaluate();
+
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(2)]
+    [DataRow(1)]
+    public void DivisionByZeroHandled(int a)
+    {
+        bool errorThrown, errorIsDivisionByZero = true;
+        _Parser.Expression = new string[] { a.ToString(), "0", "/" };
+        try
+        {
+            _Parser.Evaluate();
+            errorThrown = false;
+        }
+        catch (Exception e) when (e is DivideByZeroException)
+        {
+            errorIsDivisionByZero = true;
+            errorThrown = true;
+        }
+        catch (Exception e) when (e is not DivideByZeroException)
+        {
+            errorThrown = true;
+            errorIsDivisionByZero = false;
+            throw;
+        }
+        Assert.IsTrue(errorThrown || errorIsDivisionByZero);
+    }
+}
