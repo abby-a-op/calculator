45713235980bdab8933ac3b1b6a126ef31db55a3
Abby Ashton <ashtwj1@student.op.ac.nz>
2026-06-11 21:30:49 +1200

    Seperated token to seperate file

--- Calculator/InfixToPostfixParser.cs
+++ Calculator/InfixToPostfixParser.cs
@@ -237,17 +237,3 @@ public class InfixToPostfixParser
         return;
     }
 }
-
-public struct Token
-{
-    public string Value;
-    public TokenType Type;
-}
-
-public enum TokenType
-{
-    Invalid = -1,
-    Number,
-    Operator,
-    Function
-}
---
+++ Calculator/Token.cs
@@ -0,0 +1,15 @@
+namespace Calculator;
+
+public struct Token
+{
+    public string Value;
+    public TokenType Type;
+}
+
+public enum TokenType
+{
+    Invalid = -1,
+    Number,
+    Operator,
+    Function
+}
