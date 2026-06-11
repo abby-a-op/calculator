8318b765d9d066e736df0fef01ab3faeb4500c62
Abby Ashton <ashtwj1@student.op.ac.nz>
2026-06-11 21:09:55 +1200

    Created complete infix evaluater class

---
+++ Calculator/InfixEvaluater.cs
@@ -0,0 +1,17 @@
+namespace Calculator;
+
+public class InfixEvaluator
+{
+    private InfixToPostfixParser _infixParser = new InfixToPostfixParser();
+    private PostfixParser _postfixParser = new PostfixParser();
+
+    public string Expression;
+
+    public int Evaluate()
+    {
+        _infixParser.Expression = Expression;
+        _postfixParser.Expression = _infixParser.Parse();
+        
+        return _postfixParser.Evaluate();
+    }
+}
