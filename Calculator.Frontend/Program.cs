d25b99fb78823553f40dde7e7436d63aa48d23ab
Abby Ashton <ashtwj1@student.op.ac.nz>
2026-06-24 23:17:44 +1200

    Added extra logic to frontend

--- Calculator.Frontend/Program.cs
+++ Calculator.Frontend/Program.cs
@@ -5,15 +5,23 @@ public class Program
     public static void Main()
     {
         Interpreter interpreter = new Interpreter();
+        Console.WriteLine("Type ? or 'help' for a list of commands");
 
         while (true)
         {
             string input = Console.ReadLine() ?? "";
             interpreter.Command = input;
 
-            string result = interpreter.Run();
+            try
+            {
+                IToken result = interpreter.Run();
 
-            Console.WriteLine(result);
+                Console.WriteLine(result.Output());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
